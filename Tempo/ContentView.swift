3c5da9adb88d2371af10135c1030aad0df96bc7a
VladLiubov <91991413+VladLiubov@users.noreply.github.com>
2026-04-09 09:47:36 +0100

    Initial Commit

---
+++ Tempo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tempo
+//
+//  Created by Vladyslav on 09/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tempo/TempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoApp.swift
+//  Tempo
+//
+//  Created by Vladyslav on 09/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
